51e5167223a0b718c307908880546e3669b8a005
Matt McFarland <matmcfarland@gmail.com>
2014-06-24 14:49:09 -0500

    add date-input for turning date strings into objects

---
+++ client/components/date-input.js
@@ -0,0 +1,29 @@
+/**
+ * Convert a text field into date input
+ */
+
+Stalker.DateInputComponent = Ember.TextField.extend({
+  type: 'text',
+  placeholder: '__/__/____ __:__',
+  valueBinding: 'formattedValue',
+
+  setDate: function() {
+    var value = this.get('value');
+
+    if(value && value.match(/\d{1,2}\/\d{1,2}\/\d{4}\s+\d{1,2}:\d{2}/)) {
+      this.set('date', new Date(value));
+    } else {
+      this.set('date', null);
+    }
+  }.observes('value'),
+
+  formattedValue: function() {
+    var date = this.get('date');
+
+    if(!date) return null;
+
+    return date.getMonth() + '/' + date.getDay() + '/' +
+      date.getFullYear() + ' ' + date.getHours() + ':' +
+      (date.getMinutes() === 0 ? '00' : date.getMinutes());
+  }.property()
+});
--- client/components/index.js
+++ client/components/index.js
@@ -1,2 +1,3 @@
 // import components/modal
+// import components/date-input
 // import components/focus-input
