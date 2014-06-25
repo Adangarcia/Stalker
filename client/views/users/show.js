d52c420ec6e8136750755232f43245b3ca2c22da
Matt McFarland <matmcfarland@gmail.com>
2014-06-25 17:49:20 -0500

    add lighter background for users that are `in`

--- client/views/users/show.js
+++ client/views/users/show.js
@@ -6,7 +6,7 @@ Stalker.UserShowView = Ember.View.extend({
   tagName: 'section',
   classNames: ['user'],
   templateName: 'users/show',
-  classNameBindings: ['isEditable:editable'],
+  classNameBindings: ['isEditable:editable', 'isIn:lighten'],
   currentUserBinding: 'controller.currentUser',
 
   /**
@@ -20,6 +20,16 @@ Stalker.UserShowView = Ember.View.extend({
     return this.get('currentUser.isAdmin') ||
       this.get('content.id') === this.get('currentUser.id');
   }.property('currentUser', 'content.id'),
+
+  /**
+   * Is the user currently in?
+   *
+   * @return {Boolean}
+   */
+
+  isIn: function() {
+    return !!this.get('content.location').match(/^in$/i);
+  }.property(),
 
   /**
    * Click handler for a user
