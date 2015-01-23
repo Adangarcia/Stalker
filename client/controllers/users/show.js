9ee6798528cb9625da94a29bcd84f888ee798057
Matt McFarland <matmcfarland@gmail.com>
2015-01-23 11:03:47 -0600

    remove current user id check from isEditedByViewable

--- client/controllers/users/show.js
+++ client/controllers/users/show.js
@@ -3,9 +3,5 @@
  */
 
 Stalker.UserShowController = Ember.ObjectController.extend({
-  isEditedByViewable: function() {
-    var admin = this.get('currentUser.isSuperAdmin');
-
-    return admin && this.get('content.id') !== this.get('currentUser.id');
-  }.property('currentUser.isSuperAdmin', 'content.id')
+  isEditedByViewable: Ember.computed.oneWay('currentUser.isSuperAdmin')
 });
