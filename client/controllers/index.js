4aac35f4947262c60dd9a2bfe9fcb37699a57d1e
Matt McFarland <matmcfarland@gmail.com>
2015-07-13 11:38:24 -0500

    Revert "fix dependency, add sort change button"
    This reverts commit 103e2537afba11babb22309aaf22df4c3b40e6a8.

--- client/controllers/index.js
+++ client/controllers/index.js
@@ -20,13 +20,6 @@ Stalker.IndexController = Ember.ArrayController.extend({
 
   hasUsers: function() {
     return this.get('length') > 0;
-  }.property('length'),
-
-  toggleSort: function() {
-    if (this.sortProperties[2] === 'updated_at')
-      this.sortProperties[2] = 'name';
-    else
-      this.sortProperties[2] = 'updated_at';
-  }
+  }.property('length')
 
 });
