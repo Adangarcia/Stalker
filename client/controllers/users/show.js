916988358076fede684000b5feb16de4a0772bc1
Matt McFarland <matmcfarland@gmail.com>
2015-11-20 10:19:49 -0600

    add control for removing user as a super duper admin

--- client/controllers/users/show.js
+++ client/controllers/users/show.js
@@ -3,5 +3,15 @@
  */
 
 Stalker.UserShowController = Ember.ObjectController.extend({
-  isEditedByViewable: Ember.computed.oneWay('currentUser.isSuperAdmin')
+  isEditedByViewable: Ember.computed.oneWay('currentUser.isSuperAdmin'),
+
+  actions: {
+    destroyUser: function() {
+      var confirm = window.confirm("Are you sure?");
+
+      if(confirm) {
+        this.get("content").destroyRecord();
+      }
+    }
+  }
 });
